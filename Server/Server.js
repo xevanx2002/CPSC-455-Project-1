2ae6ee8e5965cac4e571fa85061464e24830f183
boolNoah-true <nscott16@student.rccd.edu>
2025-02-13 15:52:39 -0800

    Added multiple device communication

--- Server/Server.js
+++ Server/Server.js
@@ -1,10 +1,15 @@
 import { WebSocketServer } from 'ws';
 import express from 'express';
 
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-const httpServer = app.listen(PORT);
+const httpServer = app.listen(PORT, '0.0.0.0',() => {
+    console.log('yuh baby girl les gooooo');
+});
 const wss = new WebSocketServer({ noServer: true });
+
+const clients = new Set();
 
 function onSocketError(err) {
     console.error(err);
@@ -15,25 +20,32 @@ function heartBeat() {
     this.connected = true;
 };
 
-app.use(express.static('../Client'));
+app.use(express.static('./Client'));
 
 wss.on('connection', function connection(ws, request) {
-    const id = "2236";
+    console.log("New homie on board");
+    clients.add(ws);
 
-    console.log(`Session ID has been assigned: ${id}`)
+    // console.log(`Session ID has been assigned: ${id}`)
 
-    console.log(`Connection Established with user ${id}`);
+    console.log(`Connection Established with user `);
 
-    ws.send(`${id} has connected`);
+    // ws.send(`${id} has connected`);
 
-    wss.connected = true;
+    ws.connected = true;
     ws.on('pong', heartBeat);
 
     ws.on('error', console.error);
 
-    ws.on('message', function message(data) {
-        console.log(`${id}: ${data}`);
-        ws.send(`${id}: ${data}`);
+    ws.on('message', (message) => {
+        console.log(`Received: ${message}`);
+
+        for (const client of clients) {
+            if (client !== ws && client.readyState === ws.OPEN) {
+                client.send(`${message}`);
+                ws.send(`${message}`);
+            }
+        }
     });
 });
 
@@ -50,14 +62,15 @@ const beat = setInterval(function ping() {
 }, 30000);
 
 wss.on('close', function close () {
-    console.log(`User ${request.session.userId} has disconnected`);
+    console.log(`User has disconnected`);
+    clients.delete(ws);
     clearInterval(beat);
 });
 
 httpServer.on('upgrade', function upgrade(request, socket, head) {
     socket.on('error', onSocketError);
 
-    socket.removeListener('error', onSocketError);
+    // socket.removeListener('error', onSocketError);
 
     wss.handleUpgrade(request, socket, head, function done(ws) {
         wss.emit('connection', ws, request);
