2e2d98c14d1ebf76ce23c234b1bd0306b373ce73
Evan Wenzel <xevanx2002@csu.fullerton.edu>
2025-02-12 18:08:13 -0800

    Updated the server to utilize express.js to allow for my dynamic server control. Integration of a heart beat function into the server to maintain connection to the client. Updated client to access the server from local host rather than routing to file

--- Server/Server.js
+++ Server/Server.js
@@ -1,38 +1,70 @@
-import { createServer } from 'https';
 import { WebSocketServer } from 'ws';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const httpServer = app.listen(PORT);
+const wss = new WebSocketServer({ noServer: true });
 
 function onSocketError(err) {
     console.error(err);
-}
+};
 
-const server = createServer();
-const wss = new WebSocketServer({ port: 8080 });
+function heartBeat() {
+    console.log("This Heart has a beat");
+    this.connected = true;
+};
+
+app.use(express.static('../Client'));
+
+const client = "HelloCows";
 
 wss.on('connection', function connection(ws, request, client) {
+    const ip = request.socket.remoteAddress;
+
+    console.log(`Connection Established with user ${client}`);
+    console.log(`New user IP: ${ip}`);
+
+    ws.send(`${client} has connected`);
+
+    wss.connected = true;
+    ws.on('pong', heartBeat);
+
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
         console.log(`Recieved message ${data} from user ${client}`);
         ws.send(`Recieved message ${data} from user ${client}`);
     });
-
-    
+    ws.on('userName', function setClient(data, client) {
+        wss.client = data;
+        console.log(client);
+    });
 });
 
-server.on('upgrade', function upgrade(request, socket, head) {
+const beat = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        console.log("Heart is beating");
+        if(ws.connected === false) {
+            console.log("Heart is lost");
+            return ws.terminate();
+        }
+        ws.connected = false;
+        ws.ping();
+    });
+}, 30000);
+
+wss.on('close', function close () {
+    console.log(`User ${client} has disconnected`);
+    clearInterval(beat);
+});
+
+httpServer.on('upgrade', function upgrade(request, socket, head) {
     socket.on('error', onSocketError);
 
-    AuthenticatorResponse(request, function next(err, client) {
-        if (err || !client) {
-            socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-            socket.destory();
-            return;
-        }
+    socket.removeListener('error', onSocketError);
 
-        socket.removeListener('error', onSocketError);
-
-        wss.handleUpgrade(request, socket, head, function done(ws) {
-            wss.emit('connection', ws, request, client);
-        });
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request, client);
     });
 });
