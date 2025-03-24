452f296874bd36d02b97b69474f1fb558b607e8c
Noah Scott <149430676+BareMetal-Alchemist@users.noreply.github.com>
2025-03-23 19:49:57 -0700

    Update DB.js
    removing info

--- Server/DB.js
+++ Server/DB.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Rhune10',
+    password: 'passwordhere',
     database: 'chatdb',
     connectionLimit: 10,
     queueLimit: 0
